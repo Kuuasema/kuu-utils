985df671ce6eb0fad722699aeed3320702f9c723
JakubDobi <52402502+JakubDobi@users.noreply.github.com>
2025-09-15 11:21:37 +0300

    Additional Vector Extensions

--- Runtime/VectorExtensions.cs
+++ Runtime/VectorExtensions.cs
@@ -1,21 +1,59 @@
 using UnityEngine;
 
 namespace Kuuasema.Utils {
-    public static class VectorExtensions {
-        public static bool IsDistanceCloser(this Vector3 self, Vector3 other, float distance) {
-            float sqrDistance = distance * distance;
-            float sqrMagnitude = (self - other).sqrMagnitude;
-            return sqrMagnitude < sqrDistance;
-        }
-
-        public static bool IsPlanarDistanceCloser(this Vector3 self, Vector3 other, float distance) {
-            float sqrDistance = distance * distance;
+    public static class VectorExtensions
+    {
+        public static float PlanarDistance(this Vector3 self, Vector3 other)
+        {
             Vector3 a = self;
             Vector3 b = other;
             a.y = 0;
             b.y = 0;
-            float sqrMagnitude = (a - b).sqrMagnitude;
-            return sqrMagnitude < sqrDistance;
+            return (a - b).magnitude;
+        }
+        public static float SqrDistance(this Vector3 self, Vector3 other)
+        {
+            return (self - other).sqrMagnitude;
+        }
+        public static float SqrPlanarDistance(this Vector3 self, Vector3 other)
+        {
+            Vector3 a = self;
+            Vector3 b = other;
+            a.y = 0;
+            b.y = 0;
+            return (a - b).sqrMagnitude;
+        }
+        public static float PlanarAngle(this Vector3 self, Vector3 other)
+        {
+
+            Vector3 a = self;
+            Vector3 b = other;
+            a.y = 0;
+            b.y = 0;
+            return Vector3.Angle(a, b);
+        }
+        public static float SignedPlanarAngle(this Vector3 self, Vector3 other)
+        {
+
+            Vector3 a = self;
+            Vector3 b = other;
+            a.y = 0;
+            b.y = 0;
+            return Vector3.SignedAngle(a, b, Vector3.up);
+        }
+
+        public static bool IsDistanceCloser(this Vector3 self, Vector3 other, float distance) {
+            float sqrDistance = distance * distance;
+            return SqrDistance(self,other) < distance;
+        }
+
+        public static bool IsPlanarDistanceCloser(this Vector3 self, Vector3 other, float distance) {
+            float sqrDistance = distance * distance;
+            return SqrPlanarDistance(self,other) < sqrDistance;
+        }
+        public static bool IsPlanarAngleWithin(this Vector3 self, Vector3 other, float tolerance)
+        {
+            return PlanarAngle(self,other) <= tolerance;
         }
     }
 }
